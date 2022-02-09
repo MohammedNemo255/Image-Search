630f364013d9e1ab52f492cbacd121cc02f3521d
MohammedNemo255 <mohammednemo255@gmail.com>
2022-02-09 14:46:13 +0200

    second commit: bind views

--- app/src/main/java/com/nemodroid/searchimage/data_model/SplashImage.kt
+++ app/src/main/java/com/nemodroid/searchimage/data_model/SplashImage.kt
@@ -1,5 +1,9 @@
 package com.nemodroid.searchimage.data_model
 
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
 class SplashImage {
     var id: String? = null
     var createdAt: String? = null
@@ -15,4 +19,11 @@ class SplashImage {
     var urls: Urls? = Urls()
     var links: Links? = Links()
 
+
+    //region custom methods/binding adapters
+    @BindingAdapter(value = ["url"], requireAll = false)
+    fun setSplashImage(image: AppCompatImageView, url: String) {
+        Glide.with(image.context).load(url).into(image)
+    }
+    //endregion
 }
